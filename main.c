378a7bbef40395f0e3b9735d5a6133fa1011ae78
Keegan <kcm507@mun.ca>
2018-12-12 11:44:59 -0330

    more resources added plus new strat

--- main.c
+++ main.c
@@ -2,8 +2,50 @@
 #include <stdlib.h>
 #include <time.h>
 
+const int ROCK = 1;
+const int PAPER = 2;
+const int SCISSORS = 3;
+
+
+struct game {
+    int player1Choice;
+    int player2Choice;
+    int result;
+};
+
+struct game games[100000];
+int match = 0;
+
+int counterLastPicked(int ID) {
+    int playerID = ID;
+    int enemyLastChoice;
+    int result;
+    if(match == 0){
+        result = rand() % 3 + 1;
+    }else{
+        if(playerID == 1){
+            enemyLastChoice = games[match-1].player2Choice;
+        }
+        else{
+            enemyLastChoice = games[match-1].player1Choice;
+        }
+       switch (enemyLastChoice){
+           case 1:
+                result = PAPER;
+                break;
+            case 2:
+                result = SCISSORS;
+                break;
+            default:
+                result = ROCK;  
+                break;         
+       }
+    }
+    return result;
+}
+
 int alwaysRock(){
-    return 1;
+    return ROCK;
 }
 
 int random(){
@@ -13,24 +55,24 @@ int random(){
     return result;
 }
 
-int game(int choice1, int choice2){
+int getWinner(int choice1, int choice2){
     int result;
     if(choice1 == choice2 ){
        return result = 3;
-    }else if(choice1 == 1){
-        if(choice2 == 2){
+    }else if(choice1 == ROCK){
+        if(choice2 == PAPER){
            return result = 2;
         }else{
            return result = 1;
         }
-    } else if(choice1 == 2){
-        if(choice2 == 1){
+    } else if(choice1 == PAPER){
+        if(choice2 == ROCK){
            return result = 1;
         }else{
            return result = 2;
         }
     }else{
-        if(choice2 == 1){
+        if(choice2 == ROCK){
            return result = 2;
         }else{
            return result = 1;
@@ -44,16 +86,19 @@ int main()
     int wins2=0;
     int ties=0;
 
-    int n;
-    printf("How many games: ");
-    scanf("%d", &n);
-
+    int n = 100000;
+  
     for(int i=0; i<n; i++){
+        int id = 1;
         int result;
         int choice1 = alwaysRock();
-        int choice2 = random();
-        result = game(choice1, choice2);
+        id++;
+        int choice2 = counterLastPicked(id);
+        result = getWinner(choice1, choice2);
         printf("%d  :  %d  : %d \n",choice1,choice2,result);
+        games[i].player1Choice = choice1;
+        games[i].player2Choice = choice2;
+        games[i].result = result;
         if(result == 1){
             wins1++;
         }else if(result == 2){
@@ -61,6 +106,7 @@ int main()
         }else{
             ties++;
         }
+        match++;
     }
     printf("wins 1: %d wins 2: %d ties: %d",wins1,wins2,ties);
     return 0;
